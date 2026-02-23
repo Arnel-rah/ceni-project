ea47c5bc987cc5f72ef5682cd3844c6c20a988cd
Arnel-Rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-02-23 17:38:22 +0300

    feat(db): configure database connection with new admin user
    - Update .env with ceni_project_db_admin credentials
    - Update .gitignore to exclude sensitive files
    - Refactor DBConnection.java to use new db config
    - Update Main.java to reflect connection changes

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,2 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DBConnection {
+
+  public Connection getConnection() {
+      Dotenv dotenv = Dotenv.load();
+      try{
+          String url = dotenv.get("DB_URL");
+          String user = dotenv.get("DB_USER");
+          String password = dotenv.get("DB_PASSWORD");
+          assert url != null;
+          return DriverManager.getConnection(url, user, password);
+      }catch(SQLException e){
+          throw new RuntimeException(e);
+      }
+  }
 }
